c1641c15835b8d346181d7f8c0c304251ed4993a
niwatorinoko <rean.ogswr@gmail.com>
2024-05-12 00:35:39 +0800

    first commit

---
+++ anagram/anagram1.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+
+def solution(input_word):
+    sorted_word = "".join(sorted(input_word))
+
+    word_file = open("step2/anagram/words.txt", "r")
+    words = word_file.readlines()
+    word_file.close()
+
+    new_dictionary = defaultdict(list)
+    for word in words:
+        word = word.strip()
+        if len(word) == len(sorted_word) and word != input_word:
+            new_dictionary["".join(sorted(word))].append(word)
+    return new_dictionary[sorted_word]
+
+input_word = input()
+print(solution(input_word))
