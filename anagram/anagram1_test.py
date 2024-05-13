85a1edf79b990e1c15f134f33556424462ab765f
niwatorinoko <rean.ogswr@gmail.com>
2024-05-13 14:46:08 +0800

    add testcase

--- anagram/anagram1_test.py
+++ anagram/anagram1_test.py
@@ -4,13 +4,30 @@ from anagram1 import solution
 
 
 class TestAnagram1(unittest.TestCase):
+    #inputが空の場合、空を返すか。
     def test_should_return_None_if_input_is_empty(self):
         anagram = solution("")
-        self.assertEqual([], anagram)
+        assert anagram == []
 
+    #辞書が空の場合、空を返すか。
     def test_should_return_empty_list_if_dictionary_is_empty(self):
         anagram = solution("act")
-        self.assertEqual(["cat"], anagram)
+        assert anagram == ["cat"]
+    
+    #tcaが入力された場合、catとactを返すか。
+    def test_should_return_cat_and_act(self):
+        anagram = solution("tca")
+        assert "cat" in anagram
+        assert "act" in anagram
+
+    #
+    def test_should_return_cat_and_act_with_spaced_input_string(self):
+        anagram = solution("t ca")
+        assert anagram == []
+
+    def test_should_return_empty_if_word_too_long(self):
+        anagram = solution("Lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoio-siraiobaphetraganopterygon")
+        assert anagram == []
 
 if __name__ == '__main__':
     unittest.main()
